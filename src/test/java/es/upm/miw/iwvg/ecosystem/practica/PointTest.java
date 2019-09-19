1caba6e4818193e403a88bc1e84b84ebccd660ea
Luismoteando <LuisMiguel.Ortiz.Rozalen@alumnos.upm.es>
2019-09-19 19:44:33 +0200

    First Point improvement #11

--- src/test/java/es/upm/miw/iwvg/ecosystem/practica/PointTest.java
+++ src/test/java/es/upm/miw/iwvg/ecosystem/practica/PointTest.java
@@ -11,13 +11,14 @@ class PointTest {
 
     @BeforeEach
     void before() {
-        point = new Point(2, 3);
+        point = new Point(2, 3, 3);
     }
 
     @Test
     void testPointIntInt() {
         assertEquals(2, point.getX());
         assertEquals(3, point.getY());
+        assertEquals(3, point.getZ());
     }
 
     @Test
@@ -25,6 +26,7 @@ class PointTest {
         point = new Point(2);
         assertEquals(2, point.getX());
         assertEquals(2, point.getY());
+        assertEquals(2, point.getZ());
     }
 
     @Test
@@ -32,22 +34,35 @@ class PointTest {
         point = new Point();
         assertEquals(0, point.getX());
         assertEquals(0, point.getY());
+        assertEquals(0, point.getZ());
     }
 
     @Test
     void testModule() {
-        assertEquals(3.6055, point.module(), 10e-5);
+        assertEquals(4.6904, point.module(), 10e-5);
     }
 
     @Test
-    void testPhase() {
-        assertEquals(0.9828, point.phase(), 10e-5);
+    void testPhaseXY() {
+        assertEquals(0.9828, point.phaseXY(), 10e-5);
+    }
+
+    @Test
+    void testPhaseXZ() {
+        assertEquals(0.9828, point.phaseXZ(), 10e-5);
+    }
+
+
+    @Test
+    void testPhaseYZ() {
+        assertEquals(0.7853, point.phaseYZ(), 10e-5);
     }
 
     @Test
     void testTranslateOrigin() {
-        this.point.translateOrigin(new Point(1, 1));
+        this.point.translateOrigin(new Point(1, 1, 1));
         assertEquals(1, point.getX());
         assertEquals(2, point.getY());
+        assertEquals(2, point.getZ());
     }
 }
