0816c723ae446ef9457f588845ce58104582865b
Luismoteando <LuisMiguel.Ortiz.Rozalen@alumnos.upm.es>
2019-09-20 10:28:17 +0200

    Second User improvement #12

--- src/main/java/es/upm/miw/iwvg/ecosystem/practica/User.java
+++ src/main/java/es/upm/miw/iwvg/ecosystem/practica/User.java
@@ -4,13 +4,16 @@ public class User {
 
     private int number;
 
-    private String name;
+    private String firstName;
+
+    private String secondName;
 
     private String familyName;
 
-    public User(int number, String name, String familyName) {
+    public User(int number, String firstName, String secondName, String familyName) {
         this.number = number;
-        this.name = this.format(name);
+        this.firstName = this.format(firstName);
+        this.secondName = this.format(secondName);
         this.familyName = this.format(familyName);
     }
 
@@ -19,7 +22,7 @@ public class User {
     }
 
     public String fullName() {
-        return this.name + " " + this.familyName;
+        return this.firstName + " " + this.secondName + " " + this.familyName;
     }
 
     public String upperCaseFullName() {
@@ -27,15 +30,19 @@ public class User {
     }
 
     public String initials() {
-        return this.name.substring(0, 1) + ".";
+        return this.firstName.substring(0, 1) + "." + this.secondName.substring(0, 1) + ".";
     }
 
     public int getNumber() {
         return this.number;
     }
 
-    public String getName() {
-        return this.name;
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getSecondName() {
+        return this.secondName;
     }
 
     public String getFamilyName() {
